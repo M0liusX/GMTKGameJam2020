c76c7f506ddd77d85f829afbf54a32921f3075b8
Mario Molina <molinama@vmware.com>
2020-07-11 11:40:30 -0500

    Revamped Laws

--- Scripts/Globals/Laws.gd
+++ Scripts/Globals/Laws.gd
@@ -4,8 +4,11 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-enum Law {PLUS_ONE, STAR}
-const LAW_COUNT = 2
+enum Law {REVERSE_CONTROLS,   # REVERSES (UP/DOWN) & (LEFT/RIGHT) 
+		  MOUSE_CONTROLS,     # CHANGES CONTROL SCHEME 
+		 }
+		
+const LAW_COUNT = 2   # Update whenever you add a new law (Subtract Special Laws)
 
 var currentLaws = []
 var futureLaws = []
@@ -15,24 +18,31 @@ const queueSize = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(queueSize):
-		futureLaws.append(randi() % LAW_COUNT)
+	for _i in range(queueSize):
+		futureLaws.push_front(randi() % LAW_COUNT)
 	display()
 	
 func display():
+	print("--- Future Laws ---")
 	for law in futureLaws:
 		print_law(law)
+	print("--- Current Laws ---")
 	for law in currentLaws:
 		print_law(law)
 	
 func print_law(law):
 	match law:
-		Law.PLUS_ONE:
-			print("Law One")
-		Law.STAR:
-			print("Star")
+		Law.REVERSE_CONTROLS:
+			print("REVERSE_CONTROLS")
+		Law.MOUSE_CONTROLS:
+			print("MOUSE_CONTROLS")
 		_:
 			print("Error")
 
-func step():
-	pass
+# Takes array of possible new laws
+func step(available = range(LAW_COUNT)):
+	if currentLaws.size() == totalLaws:
+		currentLaws.pop_back()
+	currentLaws.push_front(futureLaws.pop_back())
+	futureLaws.push_front(available[randi() % available.size()])
+	display()
