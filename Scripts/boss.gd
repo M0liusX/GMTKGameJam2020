832b75ef55caffdc2a570a2a8224e7525d9fb774
Mario Molina <molinama@vmware.com>
2020-07-11 14:14:10 -0500

    Waifu update

--- Scripts/boss.gd
+++ Scripts/boss.gd
@@ -1,3 +1,4 @@
+tool
 extends KinematicBody2D
 
 
@@ -8,21 +9,86 @@ extends KinematicBody2D
 enum State {IDLE,
 }
 
+# Visual Enums
+enum Hairstyle {RANDOM = -1,
+				LONG,
+				HIME,
+				BOB,
+				TWINTAIL,
+}
+enum Haircolor {RANDOM = -1,
+				BLONDE,
+				BROWN,
+				DARK,
+				PINK,
+				SILVER,
+}
+enum Costume {RANDOM = -1,
+			HOODIE,
+			PAJAMA,
+			PE,
+			UNIFORM1,
+			UNIFORM2,
+			SWIMSUIT,
+			SUMMER,
+			TOWEL,
+			WINTER,
+}
+enum Accessory {RANDOM = -1,
+				GLASSES_BLACK,
+				CHOKER,
+				GLASSES_CIRCLE,
+				FLOWER,
+				GLASSES_RED,
+}
+
 export var health = 100
 var stepInterval = 10   # in s
 var elapsedTime = 0
 
 var currentState = State.IDLE
+
+const HAIRCOLORS = 5
+onready var BackHair = $Sprite/BackHair
+onready var FrontHair = $Sprite/FrontHair
+onready var Accessories = $Sprite/Accessories
+onready var Expressions = $Sprite/Expression
+onready var Clothes = $Sprite/Clothes
+onready var Blush = $Sprite/Blush
+
+export(Hairstyle) var hairstyle = -1
+export(Accessory) var accessory = -1
+export(Costume) var clothes = -1
+export(Haircolor) var haircolor = -1 
+var expression = -1
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
+	show_character()
+	
+func randomize_it(list, color = -1, set = -1):
+	var amount = list.size()
+	var chosen = set
+	if chosen == -1:
+		chosen = randi() % amount
+		
+	for item in list:
+		item.hide()
+	list[chosen].show()
+	
+	if color != -1:
+		for item in list[chosen].get_children():
+			item.hide()
+		list[chosen].get_children()[color].show()
+		
+	return chosen
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	elapsedTime += delta
 	process_laws()
 	state_machine()
+	if Engine.editor_hint:
+		show_character()
 	
 # Checks elapsed time and adds a new law`
 func process_laws():
@@ -43,3 +109,12 @@ func state_machine():
 func got_hit(damage):
 	health -= damage
 	
+func show_character():
+	randomize()
+	if haircolor == -1:
+		haircolor = randi() % HAIRCOLORS
+	var chosenstyle = randomize_it(BackHair.get_children(), haircolor, hairstyle)
+	randomize_it(FrontHair.get_children(), haircolor, chosenstyle)
+	randomize_it(Accessories.get_children(), -1, accessory)
+	expression = randomize_it(Expressions.get_children(), -1, expression)
+	randomize_it(Clothes.get_children(), -1, clothes)
